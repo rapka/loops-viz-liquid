3e53e61a1759eb2c605006acfd370c7f486910fe
college hill <572331+rapka@users.noreply.github.com>
2025-05-25 20:39:00 -0400

    cleanup, delete unused files

--- src/TextOverlay.js
+++ src/TextOverlay.js
@@ -37,24 +37,3 @@ TextOverlay.defaultProps = {
 }
 
 export default TextOverlay;
-
-
-// const TextRing = (text) => {
-//   const CHARS = text.split('')
-//   const INNER_ANGLE = 360 / CHARS.length
-//   return (
-//     <span
-//       className="text-ring"
-//       style={{
-//         '--total': CHARS.length,
-//         '--radius': 1 / Math.sin(INNER_ANGLE / (180 / Math.PI))
-//       }}
-//     >
-//       {CHARS.map((char, index) => (
-//         <span style={{'--index': index }}>
-//           {char}
-//         </span>
-//       ))}
-//     </span>
-//   )
-// }
